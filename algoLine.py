7deaf71acf0214523efe967a00fa8aa286e59211
Elliot <elliotcky@gmail.com>
2023-06-18 20:15:07 +0800

    Edit Algoline

--- algoLine.py
+++ algoLine.py
@@ -19,17 +19,21 @@ def algolines(ticker, days_out, wick_percent, M_max, proximity_percent):
     low_trendlines_list = []
     high_trendlines_list = []
     for trendline in low_trendlines:
+        temp_list = []
         for i in range(len(trendline[0])):
-            low_trendlines_list.append({
+            temp_list.append({
                 "index": trendline[0][i],
                 "value": trendline[1][i],
                 "time": OHLC[trendline[0][i]]['date']
             })
+            low_trendlines_list.append(temp_list)
     for trendline in high_trendlines:
+        temp_list = []
         for i in range(len(trendline[0])):
-            high_trendlines_list.append({
+            temp_list.append({
                 "index": trendline[0][i],
                 "value": trendline[1][i],
                 "time": OHLC[trendline[0][i]]['date']
             })
+            high_trendlines_list.append(temp_list)
     return low_trendlines_list, high_trendlines_list
